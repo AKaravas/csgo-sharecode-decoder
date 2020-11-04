64291f825808fef6d77696db505ad077e63bd1a4
Aristeidis <aristeidis.karavas@gmail.com>
2020-11-04 15:17:24 +0100

    initial commit

---
+++ Classes/DecodeShareCode.php
@@ -0,0 +1,148 @@
+<?php
+
+
+/**
+ * Class DecodeShareCode
+ * @package
+ */
+class DecodeShareCode
+{
+    /**
+     * @var string
+     */
+    protected $shareCodePattern = '';
+
+    /**
+     * @var string
+     */
+    protected $dictionary = '';
+
+    /**
+     * @var string
+     */
+    protected $dictionaryLength = '';
+
+    /**
+     * @var string
+     */
+    protected $cleanedShareCode = '';
+
+    /**
+     * @var array
+     */
+    protected $reversedShareCodeArray = [];
+
+    /**
+     * @param string $shareCode
+     * @return array
+     */
+    public function decode(string $shareCode): array
+    {
+        $this->fillVariables($shareCode);
+        $bigNumber = $this->getDecodedBigNumber();
+        $hexConverted = $this->convertToHex($bigNumber);
+        $bytes = $this->convertHexToBytesArray($hexConverted);
+        $matchIdBytes = array_reverse(array_slice($bytes, 0, 8));
+        $reservationIdBytes = array_reverse(array_slice($bytes, 8, 8));
+        $portBytes = array_reverse(array_slice($bytes, 16, 2));
+
+        return [
+            'matchId' =>  $getMatchId = $this->getResultFromBytes($matchIdBytes),
+            'reservationId' => $getReservationId = $this->getResultFromBytes($reservationIdBytes),
+            'tvPort' =>  $getPort = $this->getResultFromBytes($portBytes)
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getDecodedBigNumber(): string
+    {
+        $bigNumber = '';
+        foreach ($this->reversedShareCodeArray as $iValue) {
+            $bigNumber = gmp_add(
+                gmp_mul(
+                    $bigNumber,
+                    $this->dictionaryLength
+                ),
+                strpos($this->dictionary,
+                    $iValue
+                )
+            );
+        }
+        json_encode($bigNumber);
+        return  json_decode($bigNumber, true, 512, JSON_BIGINT_AS_STRING);
+    }
+
+    /**
+     * @param string $number
+     * @return string
+     */
+    public function convertToHex(string $number): string
+    {
+        $toHex = gmp_strval(gmp_init($number, 10), 16);
+
+        return  $this->padStart($toHex, '36', '0');
+
+    }
+
+    /**
+     * @param string $string
+     * @param int $length
+     * @param string $chars
+     * @return string
+     */
+    public function padStart(string $string, int $length, string $chars = ' '): string
+    {
+        return str_pad($string, $length, $chars, STR_PAD_LEFT);
+    }
+
+    /**
+     * @param string $hexString
+     * @return array
+     */
+    public function convertHexToBytesArray(string $hexString): array
+    {
+        $bytes = [];
+        $byteArray = str_split($hexString, 2);
+        foreach ($byteArray as $byte) {
+            $bytes[] = (int)base_convert($byte, 16, 10);
+        }
+
+        return $bytes;
+    }
+
+    /**
+     * @param array $bytes
+     * @return string
+     */
+    public function getResultFromBytes(array $bytes): string
+    {
+        $chars = array_map("chr", $bytes);
+        $bin = implode($chars);
+        $hex = bin2hex($bin);
+        return gmp_strval($this->gmp_hexdec($hex));
+    }
+
+    public function gmp_hexdec($n) {
+        $gmp = gmp_init(0);
+        $mult = gmp_init(1);
+        for ($i=strlen($n)-1;$i>=0;$i--,$mult=gmp_mul($mult, 16)) {
+            $gmp = gmp_add($gmp, gmp_mul($mult, hexdec($n[$i])));
+        }
+        return $gmp;
+    }
+
+    /**
+     * @param string $shareCode
+     */
+    public function fillVariables(string $shareCode): void
+    {
+        $this->shareCodePattern = "/CSGO(-?[\w]{5}){5}$/";
+        $this->dictionary = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789";
+        $this->dictionaryLength = strlen($this->dictionary);
+        $this->cleanedShareCode = str_replace(array("CSGO", "-"), "", $shareCode);
+        $this->reversedShareCodeArray = array_reverse(str_split($this->cleanedShareCode));
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# csgo-matchId-decoder
-A PHP class which decodes a CS:GO match ID
+# csgo-sharecode-decoder
+
+A PHP class which decodes a CS:GO share code ID.
+
+# Installation
+
+There is not much to install. Copy the class to your Project and add your namespace.
+
+# Usage
+
+Just call the method `decode()` on your code and pass the share code.
+
+``` php
+    $initDecodeClass = new DecodeShareCode();
+    $getInfo = $initDecodeClass->decode('CSGO-oPRbA-uTQuR-UFkiC-hYWMB-syBcO');
+   /* This returns 
+        [
+            'matchId'       =>  3418217537907720662,
+            'reservationId' =>  3418222754145501631,
+            'tvPort'        =>  34897
+        ]
+    */
+```
